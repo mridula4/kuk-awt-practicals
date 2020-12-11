5d8eab83ad768e1447117b5d179d52001f369d7e
Mridula <gabamridula@gmail.com>
2020-12-11 19:51:54 +0530

    Create string.php

---
+++ string.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+
+<body>
+
+<?php
+
+echo "My first PHP script!"; echo "<br>"; echo "<br>";
+echo nl2br("Courage is one step ahead of fear.");
+
+echo nl2br("Focus on your goal.Don't look in any direction but ahead."); echo "<br>"; echo "<br>";
+echo strtolower("The pain you feel today will be strength you feel tomorrow"); echo "<br>"; echo "<br>";
+echo strtoupper("Whatever is worrying you right now,forget about it.Take a deep breath,stay positive and know that things will get better");
+echo "<br>";  echo "<br>";
+
+echo ucwords("Believe you can and you're halfway there."); echo "<br>"; echo "<br>";
+echo "Passion first and everything will fall into place.\n";
+
+?>
+
+</body>
+
+</html>
